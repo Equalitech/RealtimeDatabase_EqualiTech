2f87418af88795f6822ca3966cb8d35a3e8c10c3
Christian B <christianbear1221@gmail.com>
2018-11-08 20:37:56 -0800

    Getting HTML basics
    _added text area
    _added Submit button
    _added print button

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
+// on window load, attach addEventListener
+// send data using textbox value
+window.onload = (() =>{
+    document.getElementById('submitButton').addEventListener('click', ()=>{
+        sendData(document.getElementById('userName').value);
+    });
+
+    document.getElementById('print').addEventListener('click', ()=>{
+        printMeOut();
+    });
+});
+
+//global firebase variable
 db = firebase.database();
 
+
+// function that sends Data to firebase realtime Database
 var newDate;
 function sendData(name){
     newDate = new Date();
@@ -7,10 +23,22 @@ function sendData(name){
     let ref = db.ref("/paterons/").push({
         name,
         date: newDate.toString(),
+    }).then(()=>{
+        console.log('data saved');
     });
+
 }
 
+
+// function that pulls all paterons into global data
 async function pullData(){
     await general('/paterons/');
     return globalData;
+}
+
+// function to spit out paterons
+async function printMeOut(){
+    await pullData();
+
+    document.getElementById('output').innerHTML = JSON.stringify(globalData);
 }
